ec1d36cb2aefe0254c2b0662e016d81c7220acfc
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-02-02 22:20:40 -0500

    Creados packages para trabajo

---
+++ src/main/java/com/pry20220262/augmentedanatomy/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.pry20220262.augmentedanatomy.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/pry20220262/augmentedanatomy/service/UserService.java
@@ -0,0 +1,4 @@
+package com.pry20220262.augmentedanatomy.service;
+
+public class UserService {
+}
