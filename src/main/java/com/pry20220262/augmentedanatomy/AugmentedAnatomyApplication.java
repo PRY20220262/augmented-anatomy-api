eca891033925536d2f0a65e36d3638c37bead94f
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-02-02 22:06:26 -0500

    Agregada conexión a la base de datos

---
+++ src/main/java/com/pry20220262/augmentedanatomy/AugmentedAnatomyApplication.java
@@ -0,0 +1,13 @@
+package com.pry20220262.augmentedanatomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugmentedAnatomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AugmentedAnatomyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pry20220262/augmentedanatomy/AugmentedAnatomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pry20220262.augmentedanatomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugmentedAnatomyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
