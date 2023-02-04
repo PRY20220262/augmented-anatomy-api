c894f80745f6a6206e00095c0613cbb6a4216757
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-02-03 21:57:26 -0500

    hu-01 Agregado user dto

---
+++ src/main/java/com/pry20220262/augmentedanatomy/resource/UserDto.java
@@ -0,0 +1,11 @@
+package com.pry20220262.augmentedanatomy.resource;
+
+public interface UserDto {
+    Long getId();
+
+    String getEmail();
+
+    String getPassword();
+
+    String getPin();
+}
