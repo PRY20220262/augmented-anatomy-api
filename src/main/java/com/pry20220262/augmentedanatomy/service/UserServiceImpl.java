b6bb19ac8aaf4216d1d8485cce5fd2c50bc701c8
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-02-03 22:11:45 -0500

    hu-01 Agregado controller de user

--- src/main/java/com/pry20220262/augmentedanatomy/service/UserServiceImpl.java
+++ src/main/java/com/pry20220262/augmentedanatomy/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.pry20220262.augmentedanatomy.service;
 import com.pry20220262.augmentedanatomy.model.User;
 import com.pry20220262.augmentedanatomy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 @Service
