a497b2a7af14b278aaffb0285410e0c2303e69b7
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-02-03 21:49:28 -0500

    hu-01 Agregada dependencia para anotaciones de notnull y notblank

--- src/main/java/com/pry20220262/augmentedanatomy/model/User.java
+++ src/main/java/com/pry20220262/augmentedanatomy/model/User.java
@@ -1,4 +1,24 @@
 package com.pry20220262.augmentedanatomy.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    private String email;
+
+    private String pin;
+
+    //relationship profile
 }
