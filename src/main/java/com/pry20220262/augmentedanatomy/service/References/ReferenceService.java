6bec81ed6746f42576387ef271cc11a1d6ed4c89
Gino Quispe Calixto <gino_21quispe@outlook.com>
2023-03-28 00:55:17 -0400

    Mejora de request de reference by referenceType y humanAnatomyId

--- src/main/java/com/pry20220262/augmentedanatomy/service/References/ReferenceService.java
+++ src/main/java/com/pry20220262/augmentedanatomy/service/References/ReferenceService.java
@@ -1,6 +1,7 @@
 package com.pry20220262.augmentedanatomy.service.References;
 
 import com.pry20220262.augmentedanatomy.model.Reference;
+import com.pry20220262.augmentedanatomy.resource.Reference.ReferenceType;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface ReferenceService {
     List<Reference> getAllReferencesByHumanAnatomyId(Long humanAnatomyId);
     List<Reference> getAllInternetReferenceByHumanAnatomyId(Long humanAnatomyId);
     List<Reference> getAllOMSReferenceByHumanAnatomyId(Long humanAnatomyId);
+    List<Reference> getAllReferenceByHumanAnatomyIdAndReferenceType(Long humanAnatomyId, ReferenceType referenceType);
     Reference updateReference(Long humanAnatomyId, Long referenceId, Reference referenceRequest);
 }
